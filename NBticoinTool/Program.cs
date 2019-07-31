47c72dc9a2c3ae98a21c39161c8eeb4ffe878ece
QuantDoShilf <shilf@quantdo.com.cn>
2019-07-31 17:18:36 +0800

    init

---
+++ NBticoinTool/Program.cs
@@ -0,0 +1,16 @@
+﻿using NBitcoin;
+using System;
+
+namespace NBticoinTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Key k1 = new Key();
+            PubKey pubk1 = k1.PubKey;
+            Console.WriteLine(pubk1);
+            Console.WriteLine(pubk1.GetAddress(Network.Main));
+        }
+    }
+}
